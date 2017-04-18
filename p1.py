2526d31ab3b0ce162954655d455954ef4a0800c4
Parya1112009 <priyankaarya013@gmail.com>
2017-04-17 18:16:28 -0700

    time
    time

--- p1.py
+++ p1.py
@@ -1,4 +1,8 @@
-import subprocess
-a = subprocess.check_output('date')
-print a
- 
+import time
+import calendar 
+#time = time.time()
+#time1 = time.clock()
+#time1 = time.localtime(time.time()) 
+time1 = time.asctime(time.localtime(time.time())) 
+#time1 = time.ctime() 
+print time1 
